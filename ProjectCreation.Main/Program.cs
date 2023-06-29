49b0eef7504a76fa283d5bcc4f70ab22a0d038f1
ArjanDeo <arjandeo@bluecomet-it.com>
2023-06-29 19:00:57 +0100

    Updated the C# Command Line Program to .NET 7.0. Added the option to create a Forge Minecraft Mod for version 1.20.1. Massively optimized code readability and performance. Created sepereate functions for tasks when deemed appropriate.

--- ProjectCreation.Main/Program.cs
+++ ProjectCreation.Main/Program.cs
@@ -1,107 +1,167 @@
 ﻿using System.Diagnostics;
+using System.IO.Compression;
+using System.Text;
+
 namespace ProjectCreation.Main
 {
-	internal class Program
-	{
-		static void Main()
-		{
-			bool validInput = false;
+    internal class Program
+    {
+        static void Main()
+        {
+            bool validInput = false;
 
-			// Introduction
-			Console.WriteLine("Welcome to the Project Creation Interface.");
+            // Introduction
+            Console.WriteLine("Welcome to the Project Creation Interface.");
 
-			while (!validInput)
-			{
-				Console.WriteLine("Please select the project you wish to create below:");
-				Console.WriteLine();
-				Console.WriteLine("1: C# Command Program (.NET 6.0)");
-				Console.WriteLine("2: Basic Website (HTML, CSS, JS)");
+            while (!validInput)
+            {
+                var sb = new StringBuilder();
 
-				string spString = Console.ReadLine();
-				int selectedProject;
+                // Selection Dialogue
+                sb.AppendLine("Please select the project you wish to create below:");
+                sb.AppendLine();
+                sb.AppendLine("1: C# Command Program (.NET 7.0)");
+                sb.AppendLine("2: Basic Website (HTML, CSS, JS)");
+                sb.AppendLine("3: Forge Minecraft Mod (1.20.1, Requires Internet Connection)");
 
-				if (!int.TryParse(spString, out selectedProject) || (selectedProject != 1 && selectedProject != 2))
-				{
-					Console.Clear();
-					Console.WriteLine("Invalid Input, please try again.");
-				}
-				else
-				{
-					validInput = true;
+                Console.WriteLine(sb.ToString());
 
-					//int selectedProject = Convert.ToInt16(Console.ReadLine());
+                string spString = Console.ReadLine();
+                int selectedProject;
 
-					// Checks user input, if user inputted 1, will execute the function for creating a c# command line program.
-					// If user inputted 2, will execute the function for creating a basic website (html, css, js).
+                if (!int.TryParse(spString, out selectedProject) || (selectedProject < 1 || selectedProject > 3))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input, please try again.");
+                }
+                else
+                {
+                    validInput = true;
 
-					switch (selectedProject)
-					{
-						case 1:
-							CommandLineInterface();
-							break;
+                    switch (selectedProject)
+                    {
+                        default:
+                            Console.WriteLine("An unexpected error occurred.");
+                            break;
+                        case 1:
+                            CommandLineInterface();
+                            break;
+                        case 2:
+                            BasicWebsite();
+                            break;
+                        case 3:
+                            MinecraftMod();
+                            break;
+                    }
+                }
+            }
+        }
 
-						case 2:
-							BasicWebsite();
-							break;
-					}
-				}
-			}
-		}
-		// Creates the files for a c# command line interface program (.net 6)
-		static void CommandLineInterface()
-		{
-			Console.WriteLine("C# Command Line Program (.NET 6.0) Selected");
-			Console.WriteLine("Enter the directory in which to create your project:");
+        static void CommandLineInterface()
+        {
+            Console.WriteLine("C# Command Line Program (.NET 7.0) Selected");
+            Console.WriteLine("Enter the directory in which to create your project:");
+            string directoryPath = Console.ReadLine();
 
-			string directoryPath = Console.ReadLine();
+            CreateDirectoryIfNotExists(directoryPath);
 
-			if (!Directory.Exists(directoryPath))
-			{
-				Directory.CreateDirectory(directoryPath);
-			}
+            Directory.SetCurrentDirectory(directoryPath); // change current directory to user input
+            Console.WriteLine("What do you want to call it?");
+            string projectName = Console.ReadLine();
+            string command = "dotnet";
+            string arguments = $"new console --language \"C#\" -n {projectName}";
 
-			Directory.SetCurrentDirectory(directoryPath); // change current directory to user input
-			Console.WriteLine("What do you want to call it?");
-			string projectname = Console.ReadLine();
-			string command = "dotnet";
-			string arguments = $"new console -n {projectname}";
+            ExecuteCommand(command, arguments);
 
-			ProcessStartInfo processInfo = new ProcessStartInfo(command, arguments);
-			processInfo.CreateNoWindow = false;
-			processInfo.UseShellExecute = false;
+            Console.WriteLine("C# Command Line Program (.NET 7.0) Created.");
+            Console.ReadKey();
+        }
 
-			Process process = Process.Start(processInfo);
-			process.WaitForExit();
-		}
+        static void BasicWebsite()
+        {
+            Console.WriteLine("Basic Website (HTML, CSS, JS) Selected");
+            Console.WriteLine("Enter the path of where you want your project:");
+            string directoryPath = Console.ReadLine();
 
-		// Creates the files for a basic website
-		static void BasicWebsite()
-		{
-			Console.WriteLine("Basic Website (HTML, CSS, JS) Selected");
-			Console.WriteLine("Enter the path of where you want your project:");
+            CreateDirectoryIfNotExists(directoryPath);
 
-			string directoryPath = Console.ReadLine();
-			// Takes the directory provided by the user and checks to see if it actually exists.
-			// If it does that it will create a variable with that directory for further use.
-			if (!Directory.Exists(directoryPath))
-			{
-				Directory.CreateDirectory(directoryPath);
-			}
+            string indexPath = Path.Combine(directoryPath, "index.html");
+            string indexHtml = "<!DOCTYPE html>\n<html>\n<head>\n\t<title>My Webpage</title>\n\t<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>\n\t<h1>Hello, world!</h1>\n\t<script src=\"main.js\"></script>\n</body>\n</html>";
+            File.WriteAllText(indexPath, indexHtml);
 
-			// write the contents of index.html
-			string indexPath = Path.Combine(directoryPath, "index.html");
-			string indexHtml = "<!DOCTYPE html>\n<html>\n<head>\n\t<title>My Webpage</title>\n\t<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>\n\t<h1>Hello, world!</h1>\n\t<script src=\"main.js\"></script>\n</body>\n</html>";
-			File.WriteAllText(indexPath, indexHtml);
+            string jsPath = Path.Combine(directoryPath, "main.js");
+            string jsCode = "console.log(\"Hello, world!\");";
+            File.WriteAllText(jsPath, jsCode);
 
-			// write the contents of main.js
-			string jsPath = Path.Combine(directoryPath, "main.js");
-			string jsCode = "console.log(\"Hello, world!\");";
-			File.WriteAllText(jsPath, jsCode);
+            string cssPath = Path.Combine(directoryPath, "style.css");
+            string cssCode = "h1 {\n\tcolor: red;\n}";
+            File.WriteAllText(cssPath, cssCode);
 
-			// write the contents of style.css
-			string cssPath = Path.Combine(directoryPath, "style.css");
-			string cssCode = "h1 {\n\tcolor: red;\n}";
-			File.WriteAllText(cssPath, cssCode);
-		}
-	}
+            Console.WriteLine("Basic Website Created.");
+            Console.ReadKey();
+        }
+
+        static void MinecraftMod()
+        {
+            string fileUrl = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.0.35/forge-1.20.1-47.0.35-mdk.zip";
+            Console.WriteLine("Forge Minecraft Mod (1.20.1) Selected");
+            Console.WriteLine("Enter the path of where you want your project:");
+            string directoryPath = Console.ReadLine();
+
+            CreateDirectoryIfNotExists(directoryPath);
+
+            int lastSlashIndex = fileUrl.LastIndexOf('/');
+            string zipPath = Path.Combine(directoryPath, fileUrl.Substring(lastSlashIndex + 1));
+
+            DownloadFile(fileUrl, zipPath);
+            ExtractZip(zipPath, directoryPath);
+
+            Console.WriteLine("Mod Directory Prepared. You will still have to generate the runs for your respective IDE (Eclipse, Intellij, etc.)");
+            Console.ReadKey();
+        }
+
+        static void CreateDirectoryIfNotExists(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        static void ExecuteCommand(string command, string arguments)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo(command, arguments);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+        }
+
+        static void DownloadFile(string fileUrl, string zipPath)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var stream = client.GetStreamAsync(fileUrl).Result)
+                {
+                    using (var fileStream = new FileStream(zipPath, FileMode.OpenOrCreate))
+                    {
+                        stream.CopyTo(fileStream);
+                    }
+                }
+            }
+        }
+
+        static void ExtractZip(string zipPath, string directoryPath)
+        {
+            try
+            {
+                ZipFile.ExtractToDirectory(zipPath, directoryPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
 }
